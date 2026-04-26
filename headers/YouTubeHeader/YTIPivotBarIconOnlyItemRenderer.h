d7c4b4867ea68f8e9ff31997ec7379e49af806a8
2K <54195182+2KGT@users.noreply.github.com>
2026-04-27 04:29:44 +0700

    Add files via upload
    Signed-off-by: 2K <54195182+2KGT@users.noreply.github.com>

---
+++ headers/YouTubeHeader/YTIPivotBarIconOnlyItemRenderer.h
@@ -0,0 +1,6 @@
+#import "YTIIcon.h"
+
+@interface YTIPivotBarIconOnlyItemRenderer : GPBMessage
+@property (nonatomic, copy, readwrite) NSString *pivotIdentifier;
+@property (nonatomic, strong, readwrite) YTIIcon *icon;
+@end
